d2ef95ac02854d40436e3fe1c0487d5052ce39a3
wangzhizhou <wangzhizhou@bytedance.com>
2025-01-05 22:03:54 +0800

    download plugins

--- Sources/HangarAPI/HangarAPIClient.swift
+++ Sources/HangarAPI/HangarAPIClient.swift
@@ -11,14 +11,14 @@ import OpenAPIURLSession
 
 /// https://hangar.papermc.io/api-docs#/
 public struct HangarAPIClient {
-
+    
     public init() {}
-
+    
     private let client = Client(
         serverURL: try! Servers.Server1.url(),
         configuration: .init(dateTranscoder: Common.ISO8601DateTranscoder()),
         transport: URLSessionTransport())
-
+    
     /// 调API前的授权接口
     /// - Parameter apiKey: https://hangar.papermc.io/ 平台帐号申请的apiKey
     /// - Returns: json web token
@@ -40,11 +40,14 @@ public struct HangarAPIClient {
         return try response.ok.body.json.result
     }
     
-    public func version(for pluginName: String, versionName: String) async throws -> PluginVersion? {
+    public func version(
+        for pluginName: String,
+        versionName: String
+    ) async throws -> PluginVersion? {
         let response = try await client.showVersion(path: .init(slugOrId: pluginName, nameOrId: versionName))
         return try response.ok.body.json
     }
-
+    
     public func latestReleaseVersion(for pluginName: String) async throws -> String? {
         let response = try await client.latestReleaseVersion(.init(path: .init(slugOrId: pluginName), headers: .init(accept: [.init(contentType: .plainText)])))
         guard case let .ok(output) = response,
@@ -55,12 +58,21 @@ public struct HangarAPIClient {
         return try await String(collecting: try output.body.plainText, upTo: Int(bytes))
     }
     
-    public func downloadPlugin(name: String, version: String, platform: PluginPlatform) async throws -> PluginJavaArchive {
+    public func downloadPlugin(
+        name: String,
+        version: String,
+        platform: PluginPlatform
+    ) async throws -> PluginJavaArchive {
         let response = try await client.downloadVersion(path: .init(slugOrId: name, nameOrId: version, platform: platform))
         return try response.ok.body.application_java_hyphen_archive
     }
     
-    public func downloadPlugin(name: String, version: String, platform: PluginPlatform, toFileURL: URL) async throws -> AsyncStream<Progress>? {
+    public func downloadPlugin(
+        name: String,
+        version: String,
+        platform: PluginPlatform,
+        toFileURL: URL
+    ) async throws -> AsyncStream<Progress>? {
         let javaArchive = try await downloadPlugin(name: name, version: version, platform: platform)
         guard case let OpenAPIRuntime.HTTPBody.Length.known(totalBytes) = javaArchive.length, totalBytes > 0
         else {
@@ -84,13 +96,19 @@ public struct HangarAPIClient {
         }
     }
     
-    public func searchPlugin(text: String, platform: PluginPlatform = .PAPER, pagination: PluginSearchPagination = .init()) async throws -> [PluginProject]? {
+    public func searchPlugin(
+        text: String,
+        platform: PluginPlatform = .PAPER,
+        pagination: PluginSearchPagination = .init()
+    ) async throws -> ([PluginProject]?, PluginPagination?)  {
         let response = try await client.getProjects(query: .init(
             pagination: pagination,
             platform: platform.rawValue,
             query: text
         ))
-        let projects = try response.ok.body.json.result
-        return projects
+        let json = try response.ok.body.json
+        let projects = json.result
+        let pagination = json.pagination
+        return (projects, pagination)
     }
 }
--- Sources/HangarAPI/Types+.swift
+++ Sources/HangarAPI/Types+.swift
@@ -4,3 +4,16 @@ public typealias PluginPlatform = Components.Schemas.Platform
 public typealias PluginJavaArchive = OpenAPIRuntime.HTTPBody
 public typealias PluginProject = Components.Schemas.Project
 public typealias PluginSearchPagination = Components.Schemas.RequestPagination
+public typealias PluginPagination = Components.Schemas.Pagination
+public typealias PluginPlatformVersionDownload = Components.Schemas.PlatformVersionDownload
+
+import Foundation
+public extension PluginPlatformVersionDownload {
+    var downloadURL: URL? {
+        guard let downloadUrl = self.downloadUrl ?? self.externalUrl
+        else {
+            return nil
+        }
+        return URL(string: downloadUrl)
+    }
+}
