a58d8d40f16c0642ac4ac42617f1dd93dddbdc85
wangzhizhou <wangzhizhou@bytedance.com>
2025-04-03 22:44:28 +0800

    update deps

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
             targets: ["HangarAPI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.6.0"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.7.0"),
+        .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.7.1"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.8.2"),
         .package(url: "https://github.com/apple/swift-openapi-urlsession.git", from: "1.0.2"),
     ],
     targets: [
